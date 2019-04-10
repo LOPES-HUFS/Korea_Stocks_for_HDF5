2c7ea730cc92c8b16fba63e149f8143fd8c00109
choosunsick <random@example.com>
2019-04-10 15:23:07 +0900

    korea_all_stocks 에 있는 파일 복사
    전체 주식 h5로 만드는 코드 추가

---
+++ make_all_stocks_h5_file.R
@@ -0,0 +1,20 @@
+library(hdf5)
+library(xts)
+
+savename <- "all_stocks.h5" #Name of file to create
+file.h5 <- H5File$new(savename, mode="w")
+
+path_list <- paste("./stocks","/",dir("./stocks"),sep="")
+
+create_h5_file<-function(filename){
+    stock_temp <- read.csv(file = filename, header = TRUE, stringsAsFactors=FALSE)
+    data_name <- paste("_",substr(filename,10,15),sep = "")
+    stock.grp <- file.h5$create_group(data_name)
+    stock.grp[['data']] <- as.matrix(stock_temp[,c(2,3,4,5,6,7)])
+    stock.grp[['index']] <- as.matrix(stock_temp[,1])
+    h5attr(stock.grp[["data"]], "colnames") <- colnames(stock_temp)[2:7]
+    h5attr(stock.grp[["index"]], "colnames") <- "Date"
+}
+
+lapply(path_list,function(x){create_h5_file(x)})
+file.h5$close_all()
--- make_h5_file.R
+++ make_h5_file.R
@@ -3,19 +3,23 @@ library(xts)
 stock_005930 <- read.csv(file = "./stocks/005930.csv", header = TRUE, stringsAsFactors=FALSE)
 stock_066570 <- read.csv(file = "./stocks/066570.csv", header = TRUE, stringsAsFactors=FALSE)
 
+savename <- "temp.h5" #Name of file to create
+file.h5 <- H5File$new(savename, mode="w")
+
+path_list <- paste("./stocks","/",dir("./stocks"),sep="")
 
 create_h5_file<-function(filename){
     stock_temp <- read.csv(file = filename, header = TRUE, stringsAsFactors=FALSE)
     data_name <- paste("_",substr(filename,10,15),sep = "")
-    savename <- "temp.h5" #Name of file to create
-    file.h5 <- H5File$new(savename, mode="w")
     stock.grp <- file.h5$create_group(data_name)
     stock.grp[['data']] <- as.matrix(stock_temp[,c(2,3,4,5,6,7)])
     stock.grp[['index']] <- as.matrix(stock_temp[,1])
     h5attr(stock.grp[["data"]], "colnames") <- colnames(stock_temp)[2:7]
     h5attr(stock.grp[["index"]], "colnames") <- "Date"
-    file.h5$close_all()
 }
+
+lapply(path_list,function(x){create_h5_file(x)})
+file.h5$close_all()
 
 # 열어서 확인 
 file.h5 <- H5File$new("temp.h5", mode="r+")
