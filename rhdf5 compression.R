d544b9eb000fe815f82658f775bae34fab1bc15d
choosunsick <random@example.com>
2019-07-02 15:21:04 +0900

    현재 주식까지 포함된 파일로 업데이트

--- rhdf5 compression.R
+++ rhdf5 compression.R
@@ -1,22 +1,43 @@
 install.packages("BiocManager")
 BiocManager::install("rhdf5")
 library(rhdf5)
+path_list <- paste("./stocks/",dir("./stocks"),sep = "")
 h5createFile("all_stock.h5")
-h5createGroup(file = "all_stock.h5",group = "colnames")
-h5write(c("Date","Open","High","Low","Close","Volume","Adj_Close"), "all_stock.h5","colnames")
+h5write(c("Date","Open","High","Low","Close","Volume","Adj_Close"), "test_all_stock.h5","colnames")
 create_h5_file<-function(filename){
     stock_temp <- read.csv(file = filename, header = TRUE, stringsAsFactors=FALSE)
-    data_name <- paste("kr",substr(filename,10,15),sep = "")
+    data_name <- paste("_",substr(filename,10,15),sep = "")
+    stock_temp$Date <- as.integer(paste(substr(stock_temp$Date,1,4),substr(stock_temp$Date,6,7),substr(stock_temp$Date,9,10),sep = ""))
     h5createGroup(file = "all_stock.h5",group = data_name)
-    h5createDataset("all_stock.h5", paste(data_name,"/data",sep=""), c(NROW(stock_temp),6),storage.mode = "integer", chunk=c(NROW(stock_temp),6), level=9)
-    h5write(as.matrix(stock_temp[,2:7]), file="all_stock.h5",name=paste(data_name,"/data",sep=""))
-    h5createDataset("all_stock.h5", paste(data_name,"/index",sep=""), c(NROW(stock_temp),1),storage.mode = "character", size=11,chunk=c(NROW(stock_temp),1), level=9)
-    h5write(as.matrix(stock_temp[,1]), file="all_stock.h5",name=paste(data_name,"/index",sep=""))
-}
+    h5createDataset(file = "all_stock.h5", dataset = paste(data_name,"/data",sep=""), dims = c(NROW(stock_temp),7),storage.mode = "integer")
+    h5write(as.matrix(stock_temp), file="test_all_stock.h5",name=paste(data_name,"/data",sep=""))
+    }
 
 lapply(path_list,function(x){create_h5_file(x)})
 
 h5closeAll()
     
+install.packages("dygraphs")
+library(dygraphs)
+library(tidyverse)
+#temp에 rownames 로 날짜 넣어줌 
+sample <- H5Fopen("all_stock.h5")
 
+samsung_sample <- sample$"kr005930"
+samsung_sample$data
+samsung_sample$index
+sample$colnames
+
+samsung_temp <- data.frame(samsung_sample$index,samsung_sample$data)
+colnames(samsung_temp) <- sample$colnames
+
+newTemp <- select(samsung_temp, "Open", "High", "Low", "Close")
+rownames(newTemp) <- samsung_temp$Date
+newTemp <- as.xts(newTemp)
+dygraph(newTemp['2019'],main = "candlestick",group = "stock_graph") %>% dyRangeSelector() %>% dyCandlestick() 
+
+temp <- select(samsung_temp, "Volume")
+rownames(temp) <- samsung_temp$Date
+temp <- as.xts(temp)
+dygraph(temp['2019'],main = "volume",group = "stock_graph") %>% dyBarChart()
 
