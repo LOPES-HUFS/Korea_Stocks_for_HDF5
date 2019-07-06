5f3199e167798ee7c47763938db68a066acbc944
choosunsick <random@example.com>
2019-07-06 16:46:47 +0900

    불필요한 파일 삭제 및 자료 갱신

--- rhdf5 compression.R
+++ rhdf5 compression.R
@@ -3,14 +3,13 @@ BiocManager::install("rhdf5")
 library(rhdf5)
 path_list <- paste("./stocks/",dir("./stocks"),sep = "")
 h5createFile("all_stock.h5")
-h5write(c("Date","Open","High","Low","Close","Volume","Adj_Close"), "test_all_stock.h5","colnames")
+h5write(c("Date","Open","High","Low","Close","Volume","Adj_Close"), "all_stock.h5","colnames")
 create_h5_file<-function(filename){
     stock_temp <- read.csv(file = filename, header = TRUE, stringsAsFactors=FALSE)
     data_name <- paste("_",substr(filename,10,15),sep = "")
     stock_temp$Date <- as.integer(paste(substr(stock_temp$Date,1,4),substr(stock_temp$Date,6,7),substr(stock_temp$Date,9,10),sep = ""))
-    h5createGroup(file = "all_stock.h5",group = data_name)
-    h5createDataset(file = "all_stock.h5", dataset = paste(data_name,"/data",sep=""), dims = c(NROW(stock_temp),7),storage.mode = "integer")
-    h5write(as.matrix(stock_temp), file="all_stock.h5",name=paste(data_name,"/data",sep=""))
+    h5createDataset(file = "all_stock.h5", dataset = data_name, dims = c(NROW(stock_temp),7),storage.mode = "integer")
+    h5write(as.matrix(stock_temp), file="all_stock.h5",name=data_name)
     }
 
 lapply(path_list,function(x){create_h5_file(x)})
